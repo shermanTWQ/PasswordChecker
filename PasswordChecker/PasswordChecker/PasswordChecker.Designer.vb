6f595fe395104e0fee3f571d88fe01f2360aa8cb
shermanTWQ <124432647+shermanTWQ@users.noreply.github.com>
2023-02-05 15:54:16 +0800

    Add files via upload

---
+++ PasswordChecker/PasswordChecker/PasswordChecker.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PasswordChecker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnVerify = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.chkUpper = New System.Windows.Forms.CheckBox()
+        Me.chkNumber = New System.Windows.Forms.CheckBox()
+        Me.chkSpecialChar = New System.Windows.Forms.CheckBox()
+        Me.chkEightChar = New System.Windows.Forms.CheckBox()
+        Me.chkCommonPassword = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Password"
+        '
+        'btnVerify
+        '
+        Me.btnVerify.Location = New System.Drawing.Point(204, 288)
+        Me.btnVerify.Name = "btnVerify"
+        Me.btnVerify.Size = New System.Drawing.Size(75, 23)
+        Me.btnVerify.TabIndex = 2
+        Me.btnVerify.Text = "Verify"
+        Me.btnVerify.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(183, 66)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtInput.Size = New System.Drawing.Size(153, 22)
+        Me.txtInput.TabIndex = 3
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(120, 268)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(240, 17)
+        Me.lblOutput.TabIndex = 7
+        Me.lblOutput.Text = "Click ""Verify"" to check your password"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(388, 34)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Check your passwords against common unscured password " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "practice or leaked passwo" &
+    "rds."
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'chkUpper
+        '
+        Me.chkUpper.AutoSize = True
+        Me.chkUpper.Enabled = False
+        Me.chkUpper.Location = New System.Drawing.Point(107, 112)
+        Me.chkUpper.Name = "chkUpper"
+        Me.chkUpper.Size = New System.Drawing.Size(156, 21)
+        Me.chkUpper.TabIndex = 9
+        Me.chkUpper.Text = "Contains uppercase"
+        Me.chkUpper.UseVisualStyleBackColor = True
+        '
+        'chkNumber
+        '
+        Me.chkNumber.AutoSize = True
+        Me.chkNumber.Enabled = False
+        Me.chkNumber.Location = New System.Drawing.Point(107, 139)
+        Me.chkNumber.Name = "chkNumber"
+        Me.chkNumber.Size = New System.Drawing.Size(144, 21)
+        Me.chkNumber.TabIndex = 10
+        Me.chkNumber.Text = "Contains numbers"
+        Me.chkNumber.UseVisualStyleBackColor = True
+        '
+        'chkSpecialChar
+        '
+        Me.chkSpecialChar.AutoSize = True
+        Me.chkSpecialChar.Enabled = False
+        Me.chkSpecialChar.Location = New System.Drawing.Point(107, 166)
+        Me.chkSpecialChar.Name = "chkSpecialChar"
+        Me.chkSpecialChar.Size = New System.Drawing.Size(204, 21)
+        Me.chkSpecialChar.TabIndex = 11
+        Me.chkSpecialChar.Text = "Contains special characters"
+        Me.chkSpecialChar.UseVisualStyleBackColor = True
+        '
+        'chkEightChar
+        '
+        Me.chkEightChar.AutoSize = True
+        Me.chkEightChar.Enabled = False
+        Me.chkEightChar.Location = New System.Drawing.Point(107, 193)
+        Me.chkEightChar.Name = "chkEightChar"
+        Me.chkEightChar.Size = New System.Drawing.Size(218, 21)
+        Me.chkEightChar.TabIndex = 12
+        Me.chkEightChar.Text = "Contains 8 characters at least"
+        Me.chkEightChar.UseVisualStyleBackColor = True
+        '
+        'chkCommonPassword
+        '
+        Me.chkCommonPassword.AutoSize = True
+        Me.chkCommonPassword.Enabled = False
+        Me.chkCommonPassword.Location = New System.Drawing.Point(107, 220)
+        Me.chkCommonPassword.Name = "chkCommonPassword"
+        Me.chkCommonPassword.Size = New System.Drawing.Size(235, 21)
+        Me.chkCommonPassword.TabIndex = 13
+        Me.chkCommonPassword.Text = "Found in common password list?"
+        Me.chkCommonPassword.UseVisualStyleBackColor = True
+        '
+        'PasswordChecker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 325)
+        Me.Controls.Add(Me.chkCommonPassword)
+        Me.Controls.Add(Me.chkEightChar)
+        Me.Controls.Add(Me.chkSpecialChar)
+        Me.Controls.Add(Me.chkNumber)
+        Me.Controls.Add(Me.chkUpper)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.btnVerify)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "PasswordChecker"
+        Me.Text = "Simple Password Checker 1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnVerify As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents chkUpper As CheckBox
+    Friend WithEvents chkNumber As CheckBox
+    Friend WithEvents chkSpecialChar As CheckBox
+    Friend WithEvents chkEightChar As CheckBox
+    Friend WithEvents chkCommonPassword As CheckBox
+End Class
---
+++ PasswordChecker/PasswordChecker/PasswordChecker.vb
@@ -0,0 +1,74 @@
+﻿Public Class PasswordChecker
+
+    ' Simple Password Checker 1.0
+    'Provided for non-commercial use. Please refer to Github Repo for more information. 
+    'Contact: Sherman_tan@outlook.com
+    '(C) Tan Wei Qiang Sherman. 
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles lblOutput.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnVerify_Click(sender As Object, e As EventArgs) Handles btnVerify.Click
+        ' Reset all checkers before using
+        chkEightChar.Checked = False
+        chkNumber.Checked = False
+        chkSpecialChar.Checked = False
+        chkUpper.Checked = False
+        chkCommonPassword.Checked = False
+
+        Dim userinput As String
+        Dim lengthcount As Integer
+        Dim commonList As New ArrayList 'Loads top 10 Most Common Passwords of 2022
+        commonList.Add("password")
+        commonList.Add("123456")
+        commonList.Add("123456789")
+        commonList.Add("guest")
+        commonList.Add("qwerty")
+        commonList.Add("12345678")
+        commonList.Add("111111")
+        commonList.Add("12345")
+        commonList.Add("12345")
+        commonList.Add("123123")
+
+        userinput = txtInput.Text.ToString 'Stores user inputs
+
+        For Each item As String In commonList 'Search if password is common
+            If (userinput = item) Then
+                chkCommonPassword.Checked = True
+            End If
+        Next
+
+        ' Triggers everytimes user click on button
+        lengthcount = Len(userinput) 'Get length of user password
+        If lengthcount >= 8 Then
+            chkEightChar.Checked = True
+        End If
+
+        For Each c As Char In userinput 'reads each input
+            If (c = "A" Or c = "B" Or c = "C" Or c = "D" Or c = "E" Or c = "F" Or c = "G" Or c = "H" Or c = "I" Or c = "J" Or c = "K" Or c = "L" Or c = "M" Or c = "N" Or c = "O" Or c = "P" Or c = "Q" Or c = "R" Or c = "S" Or c = "T" Or c = "U" Or c = "V" Or c = "W" Or c = "X" Or c = "Y" Or c = "Z") Then
+                chkUpper.Checked = True
+            ElseIf (c = "1" Or c = "2" Or c = "3" Or c = "4" Or c = "5" Or c = "6" Or c = "7" Or c = "8" Or c = "9" Or c = "0") Then
+                chkNumber.Checked = True
+            ElseIf (c = "!" Or c = "@" Or c = "#" Or c = "$" Or c = "%" Or c = "^" Or c = "&" Or c = "*" Or c = "(" Or c = ")") Then
+                chkSpecialChar.Checked = True
+            Else
+                'Skip Char as its small characters
+            End If
+        Next
+        lblOutput.Text = "Compute completed. Please see results."
+
+    End Sub
+End Class
